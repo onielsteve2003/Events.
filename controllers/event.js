dc64ae79b7b16fe4978686edece51f61704282db
stephen <stephenobumonah@gmail.com>
2023-04-10 07:27:02 +0100

    Commit

--- controllers/event.js
+++ controllers/event.js
@@ -45,6 +45,6 @@ exports.postEventData = asyncHandler(async (req, res) => {
         phoneNum: event.phoneNum,
       });
     } else {
-      res.status(400).json("Invalid User Data");
+      res.status(400).json({ success: false, error: 'Invalid' });
     }
   });
