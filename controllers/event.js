b12f23695d433a175816eabf7d7c6b302d2f8eae
stephen <stephenobumonah@gmail.com>
2023-04-10 08:08:11 +0100

    Commit

--- controllers/event.js
+++ controllers/event.js
@@ -13,42 +13,42 @@ exports.getEventData = (req, res) => {
 
 exports.postEventData = asyncHandler(async (req, res) => {
     //Get details from request
-    const { name, email, request, phoneNum } = req.body;
-  
-    //Throw error if user details is not filled
-    if (!phoneNum || !name || !email || !request) {
-      res.status(400).json({success: 'false', error: 'Please Fill in all Fields'});
-    }
-  
-    //Check if user already exists
-    const checkUser = await Event.findOne({ email });
+  const { FullName, email, request, phoneNum } = req.body;
 
-    if(err) {
-      return res.status(500).json({ success: false, error: 'Something went wrong' })
-    }
-  
-    //Throw error if user already exists
-    if (checkUser) {
-      res.status(400).json({ success: false, error: 'Seat reserved with this email already' });
-    }
-  
-    //Create or Register new User
-    const event = await Event.create({
-      name: name,
-      email: email,
-      request: request,
-      phoneNum: phoneNum,
-    });
-    res.status(200).json({code: 201, description: "Success", msg: `Thank you ${name}, you just reserved a seat!`})
-    //Send newly Created User
-    if (event) {
-      res.send({
-        name: event.name,
-        request: event.request,
-        email: event.email,
-        phoneNum: event.phoneNum,
-      });
-    } else {
-      res.status(400).json({ success: false, error: err });
-    }
+  //Throw error if user details is not filled
+  if (!phoneNum || !FullName || !email || !request) {
+    res.status(400).json({success: 'false', error: 'Please Fill in all Fields'});
+  }
+
+  //Check if user already exists
+  const checkUser = await Event.findOne({ email });
+
+  if(err) {
+    return res.status(500).json({ success: false, error: 'Something went wrong' })
+  }
+
+  //Throw error if user already exists
+  if (checkUser) {
+    res.status(400).json({ success: false, error: 'Seat reserved with this email already' });
+  }
+
+  //Create or Register new User
+  const event = await Event.create({
+    fullName: fullName,
+    email: email,
+    request: request,
+    phoneNum: phoneNum,
   });
+  res.status(200).json({code: 201, description: "Success", msg: `Thank you ${name}, you just reserved a seat!`})
+  //Send newly Created User
+  if (event) {
+    res.send({
+      fullName: event.fullName,
+      request: event.request,
+      email: event.email,
+      phoneNum: event.phoneNum,
+    });
+  } else {
+    res.status(400).json({ success: false, error: err });
+  }
+});
--- models/event.js
+++ models/event.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const eventSchema = mongoose.Schema({
-    name: {
+    fullName: {
         type: String,
         required: true
     },
